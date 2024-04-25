2d46bcd54e4435c754e2396373c8f1934f966c98
Bartek Gordon <bartekk.gordon@gmail.com>
2024-04-25 21:31:57 +0200

    Import Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return ( <p>Hello World!</p> );
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!!
+    </h1>
+  )
 }
-
-export default App;
