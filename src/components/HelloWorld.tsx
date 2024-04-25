d066791f6805961451c5f33db76f4d1cd645719e
Bartek Gordon <bartekk.gordon@gmail.com>
2024-04-25 22:16:09 +0200

    Create components

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,10 @@
+// HelloWorld.js
+import React from 'react';
+
+export default function HelloWorld() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
