70ac04b9fe547cd6695b1cfd938c9768cef524e5
Bartek Gordon <bartekk.gordon@gmail.com>
2024-04-26 13:43:10 +0200

    navbar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,7 +9,7 @@ export default function Sidebar() {
       <img
         src="https://cdn.discordapp.com/attachments/782221077119631382/1232724408473227314/IMG20240424180552.jpg?ex=662bd111&is=662a7f91&hm=81c850f33870ed1fef2cd1b15368fcce8cf65f1f70269167691b7ea0e4565842&"
         alt="Ramen"
-        className="w-32 h-32 rounded-full mx-auto"
+        className="w-32 h-32 mx-auto rounded-full"
       />
       <h1 className="my-4 text-3xl font-medium">
         <span>Bartłomiej</span> Gordon
@@ -17,8 +17,7 @@ export default function Sidebar() {
       <p className="px-2 py-1 my-3 bg-gray-200 rounded-full">Game Developer</p>
       <p>
         <a
-          className="px-2 py-1 my-3 bg-gray-200 rounded-full flex items-center justify-center"
-          href="www.google.pl"
+          className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full"
           download="name"
         >
           <GiTie className="w-6 h-6" />
@@ -26,21 +25,24 @@ export default function Sidebar() {
         </a>
       </p>
 
-      <div>
+      <div className="flex justify-center w-full my-5 space-x-8 text-black">
         <a href="https://github.com/szejkerek">
-          <AiFillGithub className="w-8 h-8" />
+          <AiFillGithub className="w-8 h-8 cursor-pointer" />
         </a>
-        <a href="www.google.pl">
-          <AiFillLinkedin className="w-8 h-8" />
+        <a href="https://www.linkedin.com/in/bartekgordon/">
+          <AiFillLinkedin className="w-8 h-8 cursor-pointer" />
         </a>
       </div>
-      <div>
-        <div>
+      <div className="py-4 my-5 bg-gray-200">
+        <div className="flex items-center justify-center space-x-2">
           <GoLocation className="w-6 h-6" />
           <span>Katowice, Gliwice</span>
         </div>
         <p>bartekk.gordon@gmail.com</p>
       </div>
+      <button className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green-400 to-blue-400">
+        Email me
+      </button>
     </div>
   );
 }
