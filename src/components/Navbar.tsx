743433f89d78cb025ce082fbe1a19e45b705a820
Bartek Gordon <bartekk.gordon@gmail.com>
2024-04-26 14:13:14 +0200

    Basic router

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,57 @@
-import React, { useState } from "react";
-// import { Link } from "react-router-dom";
+import { useEffect, useState, FunctionComponent } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
   const [activeItem, setActiveItem] = useState<string>("");
 
+  const location = useLocation();
+  const { hash, pathname, search } = location;
+
+  console.log(pathname);
+
+  useEffect(() => {
+    if (pathname === "/") handleItemClick("About");
+  }, []);
+
   const handleItemClick = (item: string) => {
     setActiveItem(item);
+  };
+
+  const NavItem: FunctionComponent<{
+    activeItem: string;
+    setActiveItem: Function;
+    name: string;
+    route: string;
+  }> = ({ activeItem, setActiveItem, name, route }) => {
+    return activeItem !== name ? (
+      <Link to={route} onClick={() => setActiveItem(name)}>
+        <span>{name}</span>
+      </Link>
+    ) : null;
   };
 
   return (
     <div>
       <span className="font-bold text-green-400">{activeItem}</span>
       <div className="flex space-x-3">
-        {/* <Link to="/" onClick={() => handleItemClick("About")}>
-          <span>About</span>
-        </Link>
-        <Link to="/projects" onClick={() => handleItemClick("Projects")}>
-          <span>Projects</span>
-        </Link>
-        <Link to="/resume" onClick={() => handleItemClick("Resume")}>
-          <span>Resume</span>
-        </Link> */}
+        <NavItem
+          setActiveItem={setActiveItem}
+          activeItem={activeItem}
+          name="About"
+          route="/"
+        />
+        <NavItem
+          setActiveItem={setActiveItem}
+          activeItem={activeItem}
+          name="Resume"
+          route="/resume"
+        />
+        <NavItem
+          setActiveItem={setActiveItem}
+          activeItem={activeItem}
+          name="Projects"
+          route="/projects"
+        />
       </div>
     </div>
   );
