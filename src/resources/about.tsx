5198d6d99dda970b5c9f5ddd13dd6dced59bcf4f
Bartek Gordon <bartekk.gordon@gmail.com>
2024-04-27 14:43:57 +0200

    Changes to resources

--- src/resources/about.tsx
+++ src/resources/about.tsx
@@ -1,5 +1,5 @@
 import { IconType } from "react-icons";
-import { FaServer } from "react-icons/fa";
+import { FaGamepad, FaCode, FaNetworkWired } from "react-icons/fa";
 
 interface IAboutService {
   title: string;
@@ -10,22 +10,38 @@ interface IAboutService {
 export const AboutService: IAboutService[] = [
   {
     title: "Game Developer",
-    about: "Doing games",
-    icon: FaServer,
+    about:
+      "Crafting immersive gaming experiences. Experienced in game jams and web development.",
+    icon: FaGamepad,
   },
   {
-    title: "Graphic Enginnerrerer",
-    about: "OpenGl itd",
-    icon: FaServer,
+    title: "Graphic Engineer",
+    about:
+      "Creating stunning visual effects with OpenGL and graphics technologies.",
+    icon: FaGamepad,
   },
   {
-    title: "Low level",
-    about: "C++ pzdr",
-    icon: FaServer,
+    title: "Low-Level Programmer",
+    about:
+      "Optimizing performance and efficiency with C++ and low-level programming.",
+    icon: FaCode,
   },
   {
-    title: "Low level",
-    about: "C++ pzdr",
-    icon: FaServer,
+    title: "Network Programmer",
+    about:
+      "Developing robust networking solutions for high-performance applications.",
+    icon: FaNetworkWired,
+  },
+  {
+    title: "AI Specialist",
+    about:
+      "Implementing intelligent algorithms for game behaviors and decision-making.",
+    icon: FaCode,
+  },
+  {
+    title: "UI/UX Designer",
+    about:
+      "Creating intuitive and visually appealing user interfaces for seamless gaming experiences.",
+    icon: FaCode,
   },
 ];
