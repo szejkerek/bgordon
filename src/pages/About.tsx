48ef3c8cf15d7367c8c950b7bedc63e276ccc83b
Bartek Gordon <bartekk.gordon@gmail.com>
2024-04-28 15:23:51 +0200

    Show about cards

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { AboutHeader } from "../resources/about";
+import { AboutHeader, AboutServices } from "../resources/about";
+import { AboutCard } from "../components/AboutCard";
 
 export default function About() {
   return (
     <div>
-      <h5>{AboutHeader.title}</h5>
+      <h5>{AboutHeader}</h5>
+      <div>
+        <h6>What I Offer</h6>
+        <div>
+          {AboutServices.map((service) => (
+            <AboutCard service={service} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/resources/about.tsx
+++ src/resources/about.tsx
@@ -1,56 +1,50 @@
 import { IconType } from "react-icons";
 import { FaGamepad, FaCode, FaNetworkWired } from "react-icons/fa";
 
-export interface IAboutService {
-  title: string;
-  about: string;
-  icon: IconType;
+export interface IAboutServices {
+  Title: string;
+  About: string;
+  Icon: IconType;
 }
 
-export interface AboutHeader {
-  title: string;
-}
+export const AboutHeader =
+  "Skilled computer science graduate with few years of experience in designing and developing games. Passionate about graphics and software development, that wants to improve his skills in Rendering, Game Design and Graphics API Programming.";
 
-export const AboutHeader: AboutHeader = {
-  title:
-    "Skilled computer science graduate with few years of experience in designing and developing games. Passionate about graphics and software development, that wants to improve his skills in Rendering, Game Design and Graphics API Programming.",
-};
-
-export const AboutService: IAboutService[] = [
+export const AboutServices: IAboutServices[] = [
   {
-    title: "Game Developer",
-    about:
+    Title: "Game Developer",
+    About:
       "Crafting immersive gaming experiences. Experienced in game jams and web development.",
-    icon: FaGamepad,
+    Icon: FaGamepad,
   },
   {
-    title: "Graphic Engineer",
-    about:
+    Title: "Graphic Engineer",
+    About:
       "Creating stunning visual effects with OpenGL and graphics technologies.",
-    icon: FaGamepad,
+    Icon: FaGamepad,
   },
   {
-    title: "Low-Level Programmer",
-    about:
+    Title: "Low-Level Programmer",
+    About:
       "Optimizing performance and efficiency with C++ and low-level programming.",
-    icon: FaCode,
+    Icon: FaCode,
   },
   {
-    title: "Network Programmer",
-    about:
+    Title: "Network Programmer",
+    About:
       "Developing robust networking solutions for high-performance applications.",
-    icon: FaNetworkWired,
+    Icon: FaNetworkWired,
   },
   {
-    title: "AI Specialist",
-    about:
+    Title: "AI Specialist",
+    About:
       "Implementing intelligent algorithms for game behaviors and decision-making.",
-    icon: FaCode,
+    Icon: FaCode,
   },
   {
-    title: "UI/UX Designer",
-    about:
+    Title: "UI/UX Designer",
+    About:
       "Creating intuitive and visually appealing user interfaces for seamless gaming experiences.",
-    icon: FaCode,
+    Icon: FaCode,
   },
 ];
