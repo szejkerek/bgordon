9e0ad85cf84a5be0691534ca766c9507c88ef852
Bartek Gordon <bartekk.gordon@gmail.com>
2024-04-29 19:15:25 +0200

    Colors to skill bars

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Languages, Tools } from "../resources/data";
+import { SkillBar } from "../components/SkillBar";
 
 export default function Skills() {
   return (
@@ -23,7 +25,20 @@ export default function Skills() {
       </div>
       <div className="grid gap-6 md:grid-cols-2">
         <div>
-          <h5 className="my-3 text-2xl font-bold">Languages and Tools</h5>
+          <h5 className="my-3 text-2xl font-bold">Languages and Frameworks</h5>
+          <div className="my-2">
+            {Languages.map((language) => (
+              <SkillBar data={language} key={language.name} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Tools</h5>
+          <div className="my-2">
+            {Tools.map((tool) => (
+              <SkillBar data={tool} key={tool.name} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
