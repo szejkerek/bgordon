2af4a90a85ba941b1d653e32ce14e212bcef4e94
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2024-04-29 11:04:14 +0200

    Create about section

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -4,11 +4,19 @@ import { IAboutServices } from "../resources/about";
 export const AboutCard: FunctionComponent<{ service: IAboutServices }> = ({
   service: { Icon, About, Title },
 }) => {
+  const createMarkup = () => {
+    return {
+      __html: About,
+    };
+  };
+
   return (
-    <div>
-      <Icon />
-      <h4>{Title}</h4>
-      <p>{About}</p>
+    <div className="flex items-center p-2 space-x-4">
+      <Icon className="text-green-400 w-9 h-9" />
+      <div>
+        <h4 className="font-bold">{Title}</h4>
+        <p dangerouslySetInnerHTML={createMarkup()} />
+      </div>
     </div>
   );
 };
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,13 +4,18 @@ import { AboutCard } from "../components/AboutCard";
 
 export default function About() {
   return (
-    <div>
-      <h5>{AboutHeader}</h5>
-      <div>
-        <h6>What I Offer</h6>
-        <div>
+    <div className="flex flex-col px-6 pt-1">
+      <h5 className="my-3 font-medium">{AboutHeader}</h5>
+      <div
+        className="flex-grow p-4 bg-gray-400 "
+        style={{ marginLeft: "-1.5rem", marginRight: "-1.5rem" }}
+      >
+        <h6 className="my-3 text-xl font-bold tracking-wide">What I Offer</h6>
+        <div className="grid gap-6 lg:grid-cols-2">
           {AboutServices.map((service) => (
-            <AboutCard service={service} />
+            <div className="bg-gray-200 rounded-lg lg:col-span-1">
+              <AboutCard service={service} />
+            </div>
           ))}
         </div>
       </div>
