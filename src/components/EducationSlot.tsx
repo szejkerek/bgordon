7db3addba64a99cf5bafad1aa62e37c31de0a4e7
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2024-05-07 13:04:38 +0200

    Move Education to component

---
+++ src/components/EducationSlot.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from "react";
+
+export const EducationSlot: FunctionComponent = () => {
+  return (
+    <div>
+      <h5 className="my-3 text-2xl font-bold">Expirience</h5>
+      <div>
+        <h5>Polsl</h5>
+        <p>Polsl inz</p>
+        <p></p>
+      </div>
+    </div>
+  );
+};
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,27 +1,14 @@
 import React from "react";
 import { Languages, Tools } from "../resources/data";
 import { SkillBar } from "../components/SkillBar";
+import { EducationSlot } from "../components/EducationSlot";
 
 export default function Skills() {
   return (
     <div className="p-4">
       <div className="grid gap-6 md:grid-cols-2">
-        <div>
-          <h5 className="my-3 text-2xl font-bold">Education</h5>
-          <div>
-            <h5>Polsl</h5>
-            <p>Polsl inz</p>
-            <p>Staszic</p>
-          </div>
-        </div>
-        <div>
-          <h5 className="my-3 text-2xl font-bold">Expirience</h5>
-          <div>
-            <h5>Polsl</h5>
-            <p>Polsl inz</p>
-            <p></p>
-          </div>
-        </div>
+        <EducationSlot />
+        <EducationSlot />
       </div>
       <div className="grid gap-6 md:grid-cols-2">
         <div>
