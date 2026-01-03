0f32864d72bae38f7a8a71ab7fcda7175682426a
Bartek Gordon <bartekk.gordon@gmail.com>
2026-01-03 21:56:39 +0100

    Up config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourusername.github.io',
-  base: '/gamedev-portfolio',
+  site: 'https://szejkerek.github.io',
+  base: '/bgordon',
   integrations: [svelte(), mdx()],
   output: 'static',
 });
