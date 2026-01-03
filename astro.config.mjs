afbee9673558bdb9ec5205072aaa52a37019460a
Bartek Gordon <bartekk.gordon@gmail.com>
2026-01-03 22:53:28 +0100

    Up

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bgordon.pl",
-  base: "/",
+  site: "https://szejkerek.github.io",
+  base: "/bgordon",
   integrations: [svelte()]
 });
